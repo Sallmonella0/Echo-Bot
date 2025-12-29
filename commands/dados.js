f3dff4e07c1481e8af695b25140e2f1dd895aa12
Sallmonella0 <joaovkbastos@gmail.com>
2025-12-29 13:19:23 -0300

    Atualizar o dados.js

--- commands/dados.js
+++ commands/dados.js
@@ -1,24 +1,24 @@
+const { parseDice } = require('../utils/diceParser');
+const { rolarDado } = require('../utils/dadosUtils');
+
 module.exports = {
   name: 'dados',
   tipo: 'padrao',
-  execute(message, conteudo) {
-    const regex = /^!(\d+)d(\d+)$/i;
-    const match = conteudo.match(regex);
 
-    if (match) {
-      const qtd = parseInt(match[1]);
-      const lados = parseInt(match[2]);
-      if (qtd < 1 || qtd > 20 || lados < 2 || lados > 100) {
-        return message.reply('Máximo 20 dados e lados entre 2 e 100.');
-      }
-      const resultados = [];
-      for (let i = 0; i < qtd; i++) {
-        resultados.push(Math.floor(Math.random() * lados) + 1);
-      }
-      const soma = resultados.reduce((a, b) => a + b, 0);
-      message.reply(`${soma}  ⟵ [${resultados.join(', ')}] ${qtd}d${lados}`);
-      return true;
-    }
-    return false;
+  execute(message, conteudo) {
+    const parsed = parseDice(conteudo);
+    if (!parsed) return false;
+
+    const { qtd, max, mod } = parsed;
+
+    const resultados = rolarDado(qtd, max);
+    const soma = resultados.reduce((a, b) => a + b, 0);
+    const total = soma + mod;
+
+    message.reply(
+      `${total} ⟵ [${resultados.join(', ')}] ${qtd}d${max}${mod ? (mod > 0 ? `+${mod}` : mod) : ''}`
+    );
+
+    return true;
   }
 };
